05ec5866748f4e1bdbf485b6df955b55f001b8c1
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-07 23:51:25 +0200

    import full calendar

--- frontend/src/Components/Calendar.tsx
+++ frontend/src/Components/Calendar.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import frLocale from "@fullcalendar/core/locales/fr";
 
 export default function Calendar() {
-  return <div>Calendar</div>;
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      locale={frLocale}
+      events={[
+        { title: "Rendez-vous 1", date: "2024-07-08" },
+        { title: "Rendez-vous 2", date: "2024-07-09" },
+      ]}
+    />
+  );
 }
