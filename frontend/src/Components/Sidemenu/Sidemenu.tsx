833959a0fb4bdefb9ce88e4f7c086f97863d6493
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-08 01:50:06 +0200

    adding sidemenu

---
+++ frontend/src/Components/Sidemenu/Sidemenu.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./Sidemenu.css";
+
+export default function Sidemenu() {
+  return (
+    <div className="sidemenu-wrapper">
+      <div>TITLE</div>
+    </div>
+  );
+}
--- frontend/src/Pages/Appointment/Appointment.tsx
+++ frontend/src/Pages/Appointment/Appointment.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import Calendar from "../../Components/Calendar/Calendar";
+import Sidemenu from "../../Components/Sidemenu/Sidemenu";
 import "./Appointment.css";
 
 export default function Appointment() {
+  const [dateSelected, isDateSelected] = useState<boolean>(true);
   return (
-    <div className="wrapper">
+    <div className="appointment-wrapper">
       <Calendar />
+      {dateSelected && <Sidemenu />}
     </div>
   );
 }
