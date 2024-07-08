50184a877af953b33146bf91df5895890657b994
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-08 23:10:02 +0200

    format date

--- frontend/src/Components/Sidemenu/Sidemenu.tsx
+++ frontend/src/Components/Sidemenu/Sidemenu.tsx
@@ -9,7 +9,18 @@ type Props = {
 export default function Sidemenu({ dateSelected }: Props) {
   return (
     <div className="sidemenu-wrapper">
-      <div>TITLE</div>
+      <div>{formatDateToFrench(dateSelected)}</div>
     </div>
   );
 }
+
+const formatDateToFrench = (date: Date | null) => {
+  if (date == null) return;
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("fr-FR", options);
+};
