20c952e790a841d2da7642a3c0519d773f19c151
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-09 00:55:27 +0200

    button + style

--- frontend/src/Components/Sidemenu/Sidemenu.tsx
+++ frontend/src/Components/Sidemenu/Sidemenu.tsx
@@ -7,20 +7,35 @@ type Props = {
 };
 
 export default function Sidemenu({ dateSelected }: Props) {
+  const schedule = generateSchedule();
   return (
     <div className="sidemenu-wrapper">
-      <div>{formatDateToFrench(dateSelected)}</div>
+      <div className="title">{formatDateToFrench(dateSelected)}</div>
+      <div className="button-container">
+        {schedule.map((val) => (
+          <button className="button">{val}</button>
+        ))}
+      </div>
     </div>
   );
 }
 
-const formatDateToFrench = (date: Date | null) => {
-  if (date == null) return;
+const formatDateToFrench = (date: Date | null): string => {
+  if (date == null) return "";
   const options: Intl.DateTimeFormatOptions = {
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  return date.toLocaleDateString("fr-FR", options);
+  const frenchDate = date.toLocaleDateString("fr-FR", options);
+  return frenchDate.charAt(0).toUpperCase() + frenchDate.slice(1);
+};
+
+const generateSchedule = (): string[] => {
+  const scheduleArray = [];
+  for (let i = 8; i <= 20; i += 2) {
+    scheduleArray.push(i + "h - " + (i + 2).toString() + "h");
+  }
+  return scheduleArray;
 };
