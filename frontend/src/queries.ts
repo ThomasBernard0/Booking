4277be3c7679cecd676168e0511c74393e3f8c1a
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-12 21:02:38 +0200

    adding schedules hooks

---
+++ frontend/src/queries.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+export function useSchedules(date: string, dateSelected: Date | null) {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [data, setData] = useState<any>([]);
+  useEffect(() => {
+    getSchedules();
+  }, [dateSelected]);
+  async function getSchedules() {
+    const response = await fetch(
+      `http://localhost:3000/appointment/schedules-list?date=${date}`
+    );
+    const data = await response.json();
+    setIsLoading(false);
+    setData(data);
+  }
+  return { data, isLoading };
+}
