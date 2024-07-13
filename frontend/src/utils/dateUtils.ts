097b4de51937a026fdb211572bddb4b5ff03d8dc
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-07-14 01:36:21 +0200

    adding + fix date tuils

--- frontend/src/utils/dateUtils.ts
+++ frontend/src/utils/dateUtils.ts
@@ -1,11 +1,22 @@
 export function formatDateToDDMMYYYY(date: Date | null): string {
-  if (date == null) return "";
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
+  if (!date) return "";
+  let dateObj: Date;
+  if (typeof date === "string") {
+    dateObj = new Date(date);
+  } else {
+    dateObj = date;
+  }
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth() + 1;
+  const year = dateObj.getFullYear();
   return `${day}-${month}-${year}`;
 }
 
+export function formatDDMMYYYYToDate(dateString: string): Date {
+  const [day, month, year] = dateString.split("-");
+  const formattedDate = new Date(`${year}-${month}-${day}`);
+  return formattedDate;
+}
 export function formatDateToFrenchLocale(date: Date | null): string {
   if (date == null) return "";
   const options: Intl.DateTimeFormatOptions = {
@@ -18,16 +29,18 @@ export function formatDateToFrenchLocale(date: Date | null): string {
   return frenchDate.charAt(0).toUpperCase() + frenchDate.slice(1);
 }
 
-export function getHours(date: Date): string {
+export function formatDDMMYYYYToFrenchLocale(dateString: string): string {
+  const date = formatDDMMYYYYToDate(dateString);
+  return formatDateToFrenchLocale(date);
+}
+
+export function getHours(date: Date | null): string {
   if (!date) return "";
   let dateObj: Date;
   if (typeof date === "string") {
     dateObj = new Date(date);
   } else {
     dateObj = date;
-  }
-  if (isNaN(dateObj.getTime())) {
-    return "";
   }
   const hour = dateObj.getUTCHours().toString();
   const minute = dateObj.getUTCMinutes().toString();
