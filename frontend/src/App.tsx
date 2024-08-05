f9a6637665f6354af252dd74351ad947461f22c4
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-08-06 00:25:58 +0200

    setup router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import Appointment from "./pages/Appointment/Appointment";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Booking from "./pages/Booking";
+import StripeMock from "./pages/StripeMock";
 
 function App() {
   return (
-    <>
-      <Appointment />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Booking />} />
+        <Route path="/payment" element={<StripeMock />} />
+      </Routes>
+    </Router>
   );
 }
 
