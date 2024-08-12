ce7055d374b9e6169b97a6e38372ca3b9046ae55
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-08-12 20:52:14 +0200

    deleting stripemock

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Booking from "./pages/Booking";
-import StripeMock from "./pages/StripeMock";
 import Success from "./pages/Success";
 import Error from "./pages/Error";
 
@@ -10,7 +9,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Booking />} />
-        <Route path="/payment" element={<StripeMock />} />
         <Route path="/success" element={<Success />} />
         <Route path="/error" element={<Error />} />
       </Routes>
