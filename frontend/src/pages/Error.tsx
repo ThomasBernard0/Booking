6ed4b698b59da2d00b4cb59a065fd88a7a357415
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-08-12 23:55:02 +0200

    clean up error page

--- frontend/src/pages/Error.tsx
+++ frontend/src/pages/Error.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 
 export default function Error() {
-  const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const sessionId = queryParams.get("session_id");
   return <div>Error</div>;
 }
