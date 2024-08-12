7d5f727a3a22790e2f8e102661fdb2bf7bf9237e
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-08-12 23:46:45 +0200

    cleaning success page

--- frontend/src/pages/Success.tsx
+++ frontend/src/pages/Success.tsx
@@ -1,22 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { confirmBookings } from "../queries";
+import React from "react";
 
 export default function Success() {
-  const location = useLocation();
-  const [sessionId, setSessionId] = useState<string | null>(null);
-  useEffect(() => {
-    const queryParams = new URLSearchParams(location.search);
-    const sessionIdValue = queryParams.get("session_id");
-    if (sessionIdValue) {
-      setSessionId(sessionIdValue);
-    }
-  }, [location.search]);
-  useEffect(() => {
-    if (sessionId) {
-      confirmBookings(sessionId);
-    }
-  }, [sessionId]);
-
   return <div>Success</div>;
 }
