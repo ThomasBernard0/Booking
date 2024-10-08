e10649dba730616700f6501a9b991c8962081666
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2024-10-09 00:16:09 +0200

    using mui calendar for responsive

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { Box } from "@mui/material";
 
 type Props = {
   setDateSelected: React.Dispatch<React.SetStateAction<Date | null>>;
@@ -9,8 +10,52 @@ type Props = {
 
 export default function Calendar({ setDateSelected }: Props) {
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <DateCalendar onChange={(value) => setDateSelected(value)} />
-    </LocalizationProvider>
+    <Box sx={{ padding: 2 }}>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <DateCalendar
+          onChange={(value) => setDateSelected(value)}
+          views={["year", "month", "day"]}
+          disablePast={true}
+          sx={{
+            width: "100%",
+            maxWidth: "600px",
+            height: "auto",
+            maxHeight: "650px",
+            "& .MuiPickersCalendarHeader-labelContainer": {
+              fontSize: {
+                xs: "1rem",
+                sm: "1.5rem",
+              },
+            },
+            "& .MuiDayCalendar-header": {
+              justifyContent: "space-around",
+            },
+            "& .MuiTypography-root": {
+              fontSize: {
+                xs: "1rem",
+                sm: "1.5rem",
+              },
+            },
+            "& .MuiDayCalendar-weekContainer": {
+              justifyContent: "space-around",
+            },
+            "& .MuiPickersDay-root": {
+              width: {
+                xs: "40px",
+                sm: "80px",
+              },
+              height: {
+                xs: "40px",
+                sm: "80px",
+              },
+              fontSize: {
+                xs: "1rem",
+                sm: "1.5rem",
+              },
+            },
+          }}
+        />
+      </LocalizationProvider>
+    </Box>
   );
 }
